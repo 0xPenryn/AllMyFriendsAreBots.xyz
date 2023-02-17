949231f5cdc4a14985b4ab6ea14ebdde5bc84639
0xPenryn <penryn@pm.me>
2023-02-17 10:59:47 -0600

    update twitter to use oauth 2.0

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -12,7 +12,8 @@ export default NextAuth({
   providers: [
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID as string,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET as string
+      clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
+      version: "2.0",
     })
   ]
 });
