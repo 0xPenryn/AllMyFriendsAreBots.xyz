9d7337cd91e5482d26f13c78a6db14c47f53c935
0xPenryn <penryn@pm.me>
2023-02-17 16:23:01 -0600

    hm

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -10,21 +10,21 @@ export default NextAuth({
       // session.user = user
       return session; // The return type will match the one returned in `useSession()`
     },
-    async jwt(token, user, account = {}, profile, isNewUser) {
-      if ( account.provider && !token[account.provider] ) {
-        token[account.provider] = {};
-      }
+    // async jwt(token, user, account = {}, profile, isNewUser) {
+    //   if ( account.provider && !token[account.provider] ) {
+    //     token[account.provider] = {};
+    //   }
 
-      if ( account.accessToken ) {
-        token[account.provider].accessToken = account.accessToken;
-      }
+    //   if ( account.accessToken ) {
+    //     token[account.provider].accessToken = account.accessToken;
+    //   }
 
-      if ( account.refreshToken ) {
-        token[account.provider].refreshToken = account.refreshToken;
-      }
+    //   if ( account.refreshToken ) {
+    //     token[account.provider].refreshToken = account.refreshToken;
+    //   }
 
-      return token;
-    },
+    //   return token;
+    // },
   },
   
   providers: [
