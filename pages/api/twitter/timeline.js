13f4fb254d74e3fb6f1441419de8353b9e37abc8
0xPenryn <penryn@pm.me>
2023-02-19 14:55:35 -0600

    Update timeline.js

--- pages/api/twitter/timeline.js
+++ pages/api/twitter/timeline.js
@@ -25,7 +25,7 @@ export default async (req, res) => {
       expansions: ['attachments.media_keys', 'attachments.poll_ids', 'referenced_tweets.id', 'author_id', 'entities.mentions.username', 'geo.place_id', 'in_reply_to_user_id', 'referenced_tweets.id.author_id' ],
       'media.fields': ['url'], 
       'user.fields': ['created_at', 'description', 'entities', 'id', 'location', 'name', 'pinned_tweet_id', 'profile_image_url', 'protected', 'public_metrics', 'url', 'username', 'verified', 'withheld'],
-      exclude: ['retweets', 'replies', 'replies_all', 'quoted_retweets', 'quoted_retweets_all'],
+      exclude: ['retweets', 'replies'],
     });
     const includes = new TwitterV2IncludesHelper(homeTimeline);
 
