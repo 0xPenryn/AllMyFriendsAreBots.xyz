c035c69746c2f9bf6c2d88fa2c41cf9b8a308db8
0xPenryn <penryn@pm.me>
2023-02-19 17:12:11 -0600

    Update TweetTimeline.tsx

--- components/TweetTimeline.tsx
+++ components/TweetTimeline.tsx
@@ -31,6 +31,33 @@ export default function TweetTimeline(): JSX.Element {
         setData(data)
         console.log("data: ", data ?? "no data")
         setLoading(false)
+        getTweets().then((tweets) => {
+          nickname = tweets[0].author.name ?? "New Placeholder";
+          name = tweets[0].author.username ?? "New Placeholder";
+          avatar = tweets[0].author.profile_image_url ?? "https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_400x400.jpg";
+          text = tweets[0].tweet.text ?? "New Placeholder";
+          date = tweets[0].tweet.created_at ?? "New Placeholder";
+      
+          return (
+            <FakeTweet config={{
+              user: {
+                nickname: nickname,
+                name: name,
+                avatar: avatar,
+                verified: false,
+                locked: false
+              },
+              display: "default",
+              text: text,
+              image: "",
+              date: date,
+              app: "Twitter for iPhone",
+              retweets: 1,
+              quotedTweets: 0,
+              likes: 5
+            }} />
+          )
+        });
       })
   }, [])
 
