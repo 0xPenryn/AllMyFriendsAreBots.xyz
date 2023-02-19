c1630e6bccfb37fb39a4ea30f4b486e7b83564f4
0xPenryn <penryn@pm.me>
2023-02-19 09:33:45 -0600

    testing

--- pages/api/twitter/timeline.js
+++ pages/api/twitter/timeline.js
@@ -5,24 +5,26 @@ import { TwitterApi } from 'twitter-api-v2';
 // https://github.com/PLhery/node-twitter-api-v2/blob/master/doc/paginators.md for reference here
 
 export default async (req, res) => {
-  
+
   try {
     const session = await getSession({ req });
-  const token = await getToken({ req });
-  
-  const client = new TwitterApi( token.access_token );
+    const token = await getToken({ req });
 
-  // console.log(session ?? 'No session');
-  // console.log(token ?? 'No token');
+    if (!session || !token) {
+      return res.status(401).json({
+        status: 'Unauthorized'
+      });
+    }
 
-  // const user = await client.currentUserV2();
-  const homeTimeline = await client.v2.homeTimeline({ exclude: 'replies' });
+    const client = new TwitterApi(token.access_token);
+    // const user = await client.currentUserV2();
+    const homeTimeline = await client.v2.homeTimeline({ exclude: 'replies' });
 
-  // console.log(JSON.stringify(user));
+    console.log(JSON.stringify(homeTimeline));
 
     return res.status(200).json({
       status: (session, 'Ok'),
-      data: homeTimeline
+      data: JSON.stringify(homeTimeline)
     });
   } catch (e) {
     return res.status(400).json({
