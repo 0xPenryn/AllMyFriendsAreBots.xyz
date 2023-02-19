60b1fd876edc6756af98ba34bb0e82b8cb99bf63
0xPenryn <penryn@pm.me>
2023-02-19 17:15:44 -0600

    Update TweetTimeline.tsx

--- components/TweetTimeline.tsx
+++ components/TweetTimeline.tsx
@@ -42,7 +42,7 @@ export default function TweetTimeline(): JSX.Element {
           avatar = tweets[0].author.profile_image_url ?? "https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_400x400.jpg";
           text = tweets[0].tweet.text ?? "New Placeholder";
           date = tweets[0].tweet.created_at ?? "New Placeholder";
-      
+          setLoading(false)
           return (
             <FakeTweet config={{
               user: {
@@ -63,7 +63,6 @@ export default function TweetTimeline(): JSX.Element {
             }} />
           )
         });
-        setLoading(false)
       })
   }, [])
 
