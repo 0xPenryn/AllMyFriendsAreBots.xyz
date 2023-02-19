b711b7507ff40e519a8f8f359b0ed00913dffe78
0xPenryn <penryn@pm.me>
2023-02-19 17:14:43 -0600

    Update TweetTimeline.tsx

--- components/TweetTimeline.tsx
+++ components/TweetTimeline.tsx
@@ -23,6 +23,12 @@ export default function TweetTimeline(): JSX.Element {
   const [isLoading, setLoading] = useState(false)
   var tweets = [] as any;
 
+  var nickname = "Placeholder";
+  var name = "Placeholder";
+  var avatar = "https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_400x400.jpg";
+  var text = "Placeholder";
+  var date = "Placeholder";
+
   useEffect(() => {
     setLoading(true)
     tweets = fetch('/api/twitter/timeline')
@@ -64,45 +70,39 @@ export default function TweetTimeline(): JSX.Element {
   if (isLoading) return <p>Loading...</p>
   if (!data) return <p>No Timeline</p>
 
-  var nickname = "Placeholder";
-  var name = "Placeholder";
-  var avatar = "https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_400x400.jpg";
-  var text = "Placeholder";
-  var date = "Placeholder";
-
   // nickname = tweets[0].author.name ?? "New Placeholder";
   // name = tweets[0].author.username ?? "New Placeholder";
   // avatar = tweets[0].author.profile_image_url ?? "https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_400x400.jpg";
   // text = tweets[0].tweet.text ?? "New Placeholder";
   // date = tweets[0].tweet.created_at ?? "New Placeholder";
 
-  getTweets().then((tweets) => {
-    nickname = tweets[0].author.name ?? "New Placeholder";
-    name = tweets[0].author.username ?? "New Placeholder";
-    avatar = tweets[0].author.profile_image_url ?? "https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_400x400.jpg";
-    text = tweets[0].tweet.text ?? "New Placeholder";
-    date = tweets[0].tweet.created_at ?? "New Placeholder";
+  // getTweets().then((tweets) => {
+  //   nickname = tweets[0].author.name ?? "New Placeholder";
+  //   name = tweets[0].author.username ?? "New Placeholder";
+  //   avatar = tweets[0].author.profile_image_url ?? "https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_400x400.jpg";
+  //   text = tweets[0].tweet.text ?? "New Placeholder";
+  //   date = tweets[0].tweet.created_at ?? "New Placeholder";
 
-    return (
-      <FakeTweet config={{
-        user: {
-          nickname: nickname,
-          name: name,
-          avatar: avatar,
-          verified: false,
-          locked: false
-        },
-        display: "default",
-        text: text,
-        image: "",
-        date: date,
-        app: "Twitter for iPhone",
-        retweets: 1,
-        quotedTweets: 0,
-        likes: 5
-      }} />
-    )
-  });
+  //   return (
+  //     <FakeTweet config={{
+  //       user: {
+  //         nickname: nickname,
+  //         name: name,
+  //         avatar: avatar,
+  //         verified: false,
+  //         locked: false
+  //       },
+  //       display: "default",
+  //       text: text,
+  //       image: "",
+  //       date: date,
+  //       app: "Twitter for iPhone",
+  //       retweets: 1,
+  //       quotedTweets: 0,
+  //       likes: 5
+  //     }} />
+  //   )
+  // });
 
   return (
     <FakeTweet config={{
