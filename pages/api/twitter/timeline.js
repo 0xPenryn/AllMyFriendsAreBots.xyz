a6b20f7074ea4ae9f68d5ebd9be60c281de86fe8
0xPenryn <penryn@pm.me>
2023-02-18 20:41:26 -0600

    ok try this

--- pages/api/twitter/timeline.js
+++ pages/api/twitter/timeline.js
@@ -1,43 +1,27 @@
 import { getSession } from 'next-auth/react';
 import { getToken } from 'next-auth/jwt';
-// import Twitter from 'twitter-lite';
 import { TwitterApi } from 'twitter-api-v2';
+
+// https://github.com/PLhery/node-twitter-api-v2/blob/master/doc/paginators.md for reference here
 
 export default async (req, res) => {
   const session = await getSession({ req });
   const token = await getToken({ req });
-
-  // for twitter-lite
-  // const client = new Twitter({
-  //   subdomain: "api", // "api" is the default (change for other subdomains)
-  //   version: "1.1", // version "1.1" is the default (change for other subdomains)
-  //   consumer_key: process.env.TWITTER_CLIENT_ID, // from Twitter.
-  //   consumer_secret: process.env.TWITTER_CLIENT_SECRET, // from Twitter.
-  //   access_token_key: token.access_token, // from your User (oauth_token)
-  //   access_token_secret: token.refresh_token, // from your User (oauth_token_secret)
-  // });  
   
   const client = new TwitterApi( token.access_token );
 
   // console.log(session ?? 'No session');
   // console.log(token ?? 'No token');
 
-  const user = await client.currentUserV2();
+  // const user = await client.currentUserV2();
+  const homeTimeline = await client.v2.homeTimeline({ exclude: 'replies' });
 
-  console.log(JSON.stringify(user));
-
-  // for twitter-lite
-  // client
-  // .get("account/verify_credentials")
-  // .then(results => {
-  //   console.log("results", results);
-  // })
-  // .catch(console.error);
+  // console.log(JSON.stringify(user));
 
   try {
     return res.status(200).json({
       status: (session, 'Ok'),
-      data: [user]
+      data: [homeTimeline]
     });
   } catch (e) {
     return res.status(400).json({
