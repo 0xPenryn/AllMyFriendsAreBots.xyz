a75ee26a2d46b4774b7e67edbc1ca136e0dc76fa
0xPenryn <penryn@pm.me>
2023-02-19 17:41:55 -0600

    clean up timeline serverless function

--- pages/api/twitter/timeline.js
+++ pages/api/twitter/timeline.js
@@ -1,15 +1,11 @@
-import { getSession } from 'next-auth/react';
 import { getToken } from 'next-auth/jwt';
-import { TweetHomeTimelineV2Paginator, TweetSearchRecentV2Paginator, TweetV2ListTweetsPaginator, TwitterApi } from 'twitter-api-v2';
+import { TwitterApi } from 'twitter-api-v2';
 import { TwitterV2IncludesHelper } from 'twitter-api-v2';
-import { data } from 'autoprefixer';
-
 
 // https://github.com/PLhery/node-twitter-api-v2/blob/master/doc/paginators.md for reference here
 
 export default async (req, res) => {
-
-  // const session = await getSession({ req });
+  
   const token = await getToken({ req });
 
   if (!token) {
@@ -20,7 +16,6 @@ export default async (req, res) => {
 
   try {
     const client = new TwitterApi(token.access_token);
-    // const user = await client.currentUserV2();
     const homeTimeline = await client.v2.homeTimeline({ 
       'tweet.fields': ['attachments', 'author_id', 'conversation_id', 'created_at', 'id', 'in_reply_to_user_id', 'lang', 'possibly_sensitive', 'referenced_tweets', 'source', 'text', 'withheld'],
       expansions: ['attachments.media_keys', 'attachments.poll_ids', 'referenced_tweets.id', 'author_id', 'entities.mentions.username', 'geo.place_id', 'in_reply_to_user_id', 'referenced_tweets.id.author_id' ],
@@ -29,26 +24,15 @@ export default async (req, res) => {
       exclude: ['retweets', 'replies'],
     });
     const includes = new TwitterV2IncludesHelper(homeTimeline);
-
     const body = [];
-
-    // console.log("homeTimeline: ", homeTimeline)
-    // console.log("first tweet: ", homeTimeline.tweets[0], includes.author(homeTimeline.tweets[0]));
     for (const tweet of homeTimeline.tweets) {
-      // console.log("tweet: ", tweet, includes.author(tweet));
       body.push({
         tweet: tweet, 
         author: includes.author(tweet)
       });
     }
 
-    // for (const fetchedTweet of homeTimeline) {
-    //   console.log("fetched: ", fetchedTweet);
-    //   console.log("processed: ", await client.v2.singleTweet(fetchedTweet.id));
-    // }
-
     return res.status(200).json(body);
-    // return new Response(homeTimeline);
   } catch (e) {
     return res.status(400).json({
       status: e.message
