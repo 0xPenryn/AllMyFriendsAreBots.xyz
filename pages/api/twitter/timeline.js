9dc1efafb37ae897c009eae3f05ae584717744d1
0xPenryn <penryn@pm.me>
2023-02-18 20:12:29 -0600

    try a diff twitter library

--- pages/api/twitter/timeline.js
+++ pages/api/twitter/timeline.js
@@ -1,29 +1,41 @@
 import { getSession } from 'next-auth/react';
 import { getToken } from 'next-auth/jwt';
-import Twitter from 'twitter-lite';
+// import Twitter from 'twitter-lite';
+import { TwitterApi } from 'twitter-api-v2';
 
 export default async (req, res) => {
   const session = await getSession({ req });
   const token = await getToken({ req });
 
-  const client = new Twitter({
-    subdomain: "api", // "api" is the default (change for other subdomains)
-    version: "1.1", // version "1.1" is the default (change for other subdomains)
-    consumer_key: process.env.TWITTER_CLIENT_ID, // from Twitter.
-    consumer_secret: process.env.TWITTER_CLIENT_SECRET, // from Twitter.
-    access_token_key: token.access_token, // from your User (oauth_token)
-    access_token_secret: token.refresh_token, // from your User (oauth_token_secret)
+  // for twitter-lite
+  // const client = new Twitter({
+  //   subdomain: "api", // "api" is the default (change for other subdomains)
+  //   version: "1.1", // version "1.1" is the default (change for other subdomains)
+  //   consumer_key: process.env.TWITTER_CLIENT_ID, // from Twitter.
+  //   consumer_secret: process.env.TWITTER_CLIENT_SECRET, // from Twitter.
+  //   access_token_key: token.access_token, // from your User (oauth_token)
+  //   access_token_secret: token.refresh_token, // from your User (oauth_token_secret)
+  // });  
+  
+  const client = new TwitterApi({
+    appKey: process.env.TWITTER_CLIENT_ID, // from Twitter.
+    appSecret: process.env.TWITTER_CLIENT_SECRET, // from Twitter.
+    accessToken: token.access_token, // from your User (oauth_token)
+    accessSecret: token.refresh_token, // from your User (oauth_token_secret)
   });
 
-  console.log(session ?? 'No session');
-  console.log(token ?? 'No token');
+  // console.log(session ?? 'No session');
+  // console.log(token ?? 'No token');
 
-  client
-  .get("account/verify_credentials")
-  .then(results => {
-    console.log("results", results);
-  })
-  .catch(console.error);
+  console.log(client.currentUser ?? 'No client user');
+
+  // for twitter-lite
+  // client
+  // .get("account/verify_credentials")
+  // .then(results => {
+  //   console.log("results", results);
+  // })
+  // .catch(console.error);
 
   try {
     return res.status(200).json({
