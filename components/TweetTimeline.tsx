37af5d27a3a75f18ca8a48d1add8d1be75dbdb28
0xPenryn <penryn@pm.me>
2023-02-19 18:42:13 -0600

    alsdfjaskl

--- components/TweetTimeline.tsx
+++ components/TweetTimeline.tsx
@@ -1,4 +1,4 @@
-import { TweetHomeTimelineV2Paginator, TwitterApi, TwitterV2IncludesHelper } from 'twitter-api-v2';
+import { TweetHomeTimelineV2Paginator, TweetV2, TwitterApi, TwitterV2IncludesHelper } from 'twitter-api-v2';
 import FakeTweet from "fake-tweet";
 import { useState, useEffect } from "react";
 
@@ -15,9 +15,6 @@ const getTweets = async () => {
 
 };
 
-
-var tweets = [] as any;
-
 var nickname = "Placeholder";
 var name = "Placeholder";
 var avatar = "https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_400x400.jpg";
@@ -26,108 +23,27 @@ var date = "Placeholder";
 
 export default function TweetTimeline(): JSX.Element {
 
-  const [data, setData] = useState<any[]>([])
-  const [isLoading, setLoading] = useState(false)
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true)
-    tweets = fetch('/api/twitter/timeline')
+    fetch('/api/twitter/timeline')
       .then((res) => res.json())
       .then((data) => {
         setData(data)
-        // console.log("data: ", data ?? "no data")
-        // nickname = data[0].author.name ?? "New Placeholder";
-        // name = data[0].author.username ?? "New Placeholder";
-        // avatar = data[0].author.profile_image_url ?? "https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_400x400.jpg";
-        // text = data[0].tweet.text ?? "New Placeholder";
-        // date = data[0].tweet.created_at ?? "New Placeholder";
-        setLoading(false)
-        // return (
-        //   <FakeTweet config={{
-        //     user: {
-        //       nickname: nickname,
-        //       name: name,
-        //       avatar: avatar,
-        //       verified: false,
-        //       locked: false
-        //     },
-        //     display: "default",
-        //     text: text,
-        //     image: "",
-        //     date: date,
-        //     app: "Twitter for iPhone",
-        //     retweets: 1,
-        //     quotedTweets: 0,
-        //     likes: 5
-        //   }} />
-        // )
-      });
-  }, [data]);
-
-  if (isLoading) return <p>Loading...</p>
-  if (!isLoading && data) {
-        console.log("data: ", data ?? "no data")
+        console.log("data: ", data)
         nickname = data[0].author.name ?? "New Placeholder";
         name = data[0].author.username ?? "New Placeholder";
         avatar = data[0].author.profile_image_url ?? "https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_400x400.jpg";
         text = data[0].tweet.text ?? "New Placeholder";
         date = data[0].tweet.created_at ?? "New Placeholder";
-        // setLoading(false)
-        return (
-          <FakeTweet config={{
-            user: {
-              nickname: nickname,
-              name: name,
-              avatar: avatar,
-              verified: false,
-              locked: false
-            },
-            display: "default",
-            text: text,
-            image: "",
-            date: date,
-            app: "Twitter for iPhone",
-            retweets: 1,
-            quotedTweets: 0,
-            likes: 5
-          }} />
-        )
-  }
-  if (!data) return <p>No Timeline</p>
+        setLoading(false)
+      })
+  }, [])
 
-  // nickname = tweets[0].author.name ?? "New Placeholder";
-  // name = tweets[0].author.username ?? "New Placeholder";
-  // avatar = tweets[0].author.profile_image_url ?? "https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_400x400.jpg";
-  // text = tweets[0].tweet.text ?? "New Placeholder";
-  // date = tweets[0].tweet.created_at ?? "New Placeholder";
-
-  // getTweets().then((tweets) => {
-  //   nickname = tweets[0].author.name ?? "New Placeholder";
-  //   name = tweets[0].author.username ?? "New Placeholder";
-  //   avatar = tweets[0].author.profile_image_url ?? "https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_400x400.jpg";
-  //   text = tweets[0].tweet.text ?? "New Placeholder";
-  //   date = tweets[0].tweet.created_at ?? "New Placeholder";
-
-  //   return (
-  //     <FakeTweet config={{
-  //       user: {
-  //         nickname: nickname,
-  //         name: name,
-  //         avatar: avatar,
-  //         verified: false,
-  //         locked: false
-  //       },
-  //       display: "default",
-  //       text: text,
-  //       image: "",
-  //       date: date,
-  //       app: "Twitter for iPhone",
-  //       retweets: 1,
-  //       quotedTweets: 0,
-  //       likes: 5
-  //     }} />
-  //   )
-  // });
+  if (loading) return <p>Loading Timeline...</p>
+  if (!data) return <p>No timeline</p>
 
   return (
     <FakeTweet config={{
