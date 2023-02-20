a3d2e10b43b93ff21e2132e096eef7c825702247
0xPenryn <penryn@pm.me>
2023-02-20 16:19:44 -0600

    eee

--- components/TweetTimeline.tsx
+++ components/TweetTimeline.tsx
@@ -16,6 +16,52 @@ interface TweetTimeline {
   tweetNumber: number;
 }
 
+// const [tweetReal, setTweetReal] = useState("");
+// const [loadingOAI, setLoadingOAI] = useState(false);
+// const [tweetAI, setTweetAI] = useState<String>("");
+
+// const prompt =
+//   `Generate 2 funny twitter bios with no hashtags and clearly labeled "1." and "2.". Make sure there is a joke in there and it's a little ridiculous. Make sure each generated bio is at max 20 words and base it on this context:`;
+
+// const generateTweet = async (e: any) => {
+//   e.preventDefault();
+//   setTweetAI("");
+//   setLoadingOAI(true);
+//   const response = await fetch("/api/generate", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       prompt,
+//     }),
+//   });
+//   console.log("Edge function returned.");
+
+//   if (!response.ok) {
+//     throw new Error(response.statusText);
+//   }
+
+//   // This data is a ReadableStream
+//   const data = response.body;
+//   if (!data) {
+//     return;
+//   }
+
+//   const reader = data.getReader();
+//   const decoder = new TextDecoder();
+//   let done = false;
+
+//   while (!done) {
+//     const { value, done: doneReading } = await reader.read();
+//     done = doneReading;
+//     const chunkValue = decoder.decode(value);
+//     setTweetAI((prev) => prev + chunkValue);
+//   }
+
+//   setLoadingOAI(false);
+// };
+
 export default function TweetTimeline({ tweetNumber }: TweetTimeline): JSX.Element {
 
   const [data, setData] = useState([] as Array<any>);
@@ -31,19 +77,19 @@ export default function TweetTimeline({ tweetNumber }: TweetTimeline): JSX.Eleme
         setData(data)
         console.log("first data: ", data)
         console.log("first tweetNumber: ", tweetNumber)
-        nickname = data[tweetNumber].author.username ?? "New Placeholder";
+        nickname = data[tweetNumber]?.author.username ?? "New Placeholder";
         console.log("first nickname: ", nickname)
-        name = data[tweetNumber].author.name ?? "New Placeholder";
-        avatar = data[tweetNumber].author.profile_image_url ?? "https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_400x400.jpg";
-        text = data[tweetNumber].tweet.text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '').replace(/&amp;/g, "&") ?? "New Placeholder";
+        name = data[tweetNumber]?.author.name ?? "New Placeholder";
+        avatar = data[tweetNumber]?.author.profile_image_url ?? "https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_400x400.jpg";
+        text = data[tweetNumber]?.tweet.text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, "").replace(/&amp;/g, "&") || "New Placeholder";
         image = [];
-        for (let i = 0; i < data[tweetNumber].tweet?.attachments?.media_keys.length ?? 0; i++) {
+        for (let i = 0; i < data[tweetNumber]?.tweet?.attachments?.media_keys.length ?? 0; i++) {
           image.push(data[tweetNumber].media[i].url)
         }
-        date = Date.parse(data[tweetNumber].tweet.created_at) ?? "New Placeholder";
-        retweets = data[tweetNumber].tweet.public_metrics.retweet_count ?? 10;
-        quotedTweets = data[tweetNumber].tweet.public_metrics.quote_count ?? 10;
-        likes = data[tweetNumber].tweet.public_metrics.like_count ?? 10;
+        date = Date.parse(data[tweetNumber]?.tweet.created_at) ?? "New Placeholder";
+        retweets = data[tweetNumber]?.tweet.public_metrics.retweet_count ?? 10;
+        quotedTweets = data[tweetNumber]?.tweet.public_metrics.quote_count ?? 10;
+        likes = data[tweetNumber]?.tweet.public_metrics.like_count ?? 10;
         // setLoading(false)
         setLoading(false)
         // setHandoff(true)
@@ -53,43 +99,24 @@ export default function TweetTimeline({ tweetNumber }: TweetTimeline): JSX.Eleme
 
   useEffect(() => {
     // if (!handoff) return () => {<p>what the hell is going on</p>}
-    if (loading) return () => {<p>Loading Tweet...</p>}
-    if (!data) return () => {<p>No Tweet :/</p>}
+    if (loading) return () => { <p>Loading Tweet...</p> }
+    if (!data) return () => { <p>No Tweet :/</p> }
     // console.log("second data: ", data)
     console.log("second tweetNumber: ", tweetNumber)
-    nickname = data[tweetNumber].author.username ?? "New Placeholder";
+    nickname = data[tweetNumber]?.author.username ?? ["New Placeholder ", tweetNumber];
     console.log("second nickname: ", nickname)
-    name = data[tweetNumber].author.name ?? "New Placeholder";
-    avatar = data[tweetNumber].author.profile_image_url ?? "https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_400x400.jpg";
-    text = data[tweetNumber].tweet.text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '').replace(/&amp;/g, "&") ?? "New Placeholder";
+    name = data[tweetNumber]?.author.name ?? ["New Placeholder ", tweetNumber];
+    avatar = data[tweetNumber]?.author.profile_image_url ?? "https://pbs.twimg.com/profile_images/1488548719062654976/u6qfBBkF_400x400.jpg";
+    text = data[tweetNumber]?.tweet.text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, "").replace(/&amp;/g, "&") || ["Placeholder Tweet ", tweetNumber];
     image = [];
-    for (let i = 0; i < data[tweetNumber].tweet?.attachments?.media_keys.length ?? 0; i++) {
-      image.push(data[tweetNumber].media[i].url)
+    for (let i = 0; i < data[tweetNumber]?.tweet?.attachments?.media_keys.length ?? 0; i++) {
+      image.push(data[tweetNumber]?.media[i].url)
     }
-    date = Date.parse(data[tweetNumber].tweet.created_at) ?? "New Placeholder";
-    retweets = data[tweetNumber].tweet.public_metrics.retweet_count ?? 10;
-    quotedTweets = data[tweetNumber].tweet.public_metrics.quote_count ?? 10;
-    likes = data[tweetNumber].tweet.public_metrics.like_count ?? 10;
+    date = Date.parse(data[tweetNumber]?.tweet.created_at) ?? "New Placeholder";
+    retweets = data[tweetNumber]?.tweet.public_metrics.retweet_count ?? 10;
+    quotedTweets = data[tweetNumber]?.tweet.public_metrics.quote_count ?? 10;
+    likes = data[tweetNumber]?.tweet.public_metrics.like_count ?? 10;
     console.log("end of second effect")
-    return () => {
-      <FakeTweet config={{
-        user: {
-          nickname: nickname,
-          name: name,
-          avatar: avatar,
-          verified: false,
-          locked: false
-        },
-        display: "default",
-        text: text,
-        image: image,
-        date: new Date(date).toLocaleString('en-US'),
-        app: "Twitter for AI effect",
-        retweets: retweets,
-        quotedTweets: quotedTweets,
-        likes: likes
-      }} />
-    }
   }, [tweetNumber, handoff, data])
 
   if (loading) return <p>Loading Tweet...</p>
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,16 +6,8 @@ import TweetTimeline from "../components/TweetTimeline";
 
 const Home: NextPage = () => {
   const { data: session, status } = useSession();
-
   const [tweetIndex, setTweetIndex] = useState(0);
-
-  // const getTweets = async () => {
-  //   const homeTimeline = await fetch("/api/twitter/timeline")
-  //     .then(
-  //       (response) => response.json()
-  //     );
-  //   return await homeTimeline;
-  // };
+  const [score, setScore] = useState(0);
 
   {/* I DO NOT KNOW WHAT I AM DOING DO NOT CRITICIZE ME */}
 
@@ -26,15 +18,19 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {!session && <>
-        Not signed in.<br />
+        {/* Not signed in.<br /> */}
+        <h1>Personhood is hard to prove. <br /></h1>
         <button onClick={() => signIn("twitter")}>Sign in with Twitter</button>
       </>}
-      {session && <>
-        Signed in as {session.user?.name ?? "no user object"} <br />
-        <button onClick={() => signOut()}>Sign out</button> <br />
-        <button onClick={() => setTweetIndex(tweetIndex + 1)}>Next Tweet!</button> <br />
-        <TweetTimeline tweetNumber={tweetIndex} />
-      </>}
+      {/* {session && <> */}
+      Signed in as {session?.user?.name ?? "no user object"} <br />
+      <button onClick={() => signOut()}>Sign out</button> <br />
+      <TweetTimeline tweetNumber={tweetIndex} />
+      <div className="flex flex-row">
+        <button className="mx-5" onClick={() => setTweetIndex(tweetIndex + 1)}>Human</button>
+        <button className="mx-5" onClick={() => setTweetIndex(tweetIndex + 1)}>AI</button>
+      </div>
+      {/* </>} */}
     </div>
   );
 };
