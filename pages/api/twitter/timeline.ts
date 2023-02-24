53341cdf2642a0e6ec9e0bb27e4259195176a5a8
0xPenryn <penryn@pm.me>
2023-02-23 23:39:17 -0700

    Update timeline.ts

--- pages/api/twitter/timeline.ts
+++ pages/api/twitter/timeline.ts
@@ -27,7 +27,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       'media.fields': ['url'],
       'user.fields': ['created_at', 'description', 'entities', 'id', 'location', 'name', 'pinned_tweet_id', 'profile_image_url', 'protected', 'public_metrics', 'url', 'username', 'verified', 'withheld'],
       exclude: ['retweets', 'replies'],
-      'until_id': req.body,
+      'until_id': req.body.until_id,
     });
   } else {
     homeTimeline = await client.v2.homeTimeline({
@@ -36,7 +36,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       'media.fields': ['url'],
       'user.fields': ['created_at', 'description', 'entities', 'id', 'location', 'name', 'pinned_tweet_id', 'profile_image_url', 'protected', 'public_metrics', 'url', 'username', 'verified', 'withheld'],
       exclude: ['retweets', 'replies'],
-      until_id: '20765432100123456789',
     });
   }
 
