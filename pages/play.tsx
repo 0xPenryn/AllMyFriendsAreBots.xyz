75b6fc1601aeab9512f28a078add4e79dfa0b518
0xPenryn <penryn@pm.me>
2023-02-24 00:40:58 -0700

    Revert "Update play.tsx"
    This reverts commit c9c64f1990091016989af8fc4340e6d08e8553ba.

--- pages/play.tsx
+++ pages/play.tsx
@@ -21,32 +21,11 @@ const Play: NextPage = () => {
   const [tweets, setTweets] = useState<TweetConfig[]>([]);
   const [loading, setLoading] = useState(true);
 
-  var tweet: TweetConfig = {
-    user: {
-      nickname: "string",
-      name: "str",
-      avatar: "string",
-      verified: false,
-      locked: false,
-    },
-    display: "default",
-    text: "string",
-    image: [],
-    date: "string",
-    app: "Twitter for AI",
-    retweets: -1,
-    quotedTweets: -1,
-    likes: -1,
-    AI: false,
-    id: '0',
-  }
-
   useEffect(() => {
     setHighScore(parseInt(localStorage.getItem("highScore") ?? "0"));
     loadTweets().then((tweets) => {
       setTweets(tweets);
       setLoading(false);
-      tweet = tweets.shift()!;
     })
   }, [])
 
@@ -65,10 +44,27 @@ const Play: NextPage = () => {
     if (tweets.length == 2 && !loading) {
       loadMoreTweets();
     }
-    tweet = tweets.shift()!;
-  }, [tweetId, tweet])
+  }, [tweetId])
 
-  
+  var tweet: TweetConfig = {
+    user: {
+        nickname: "string",
+        name: "str",
+        avatar: "string",
+        verified: false,
+        locked: false,
+    },
+    display: "default",
+    text: "string",
+    image: [],
+    date: "string",
+    app: "Twitter for AI",
+    retweets: -1,
+    quotedTweets: -1,
+    likes: -1,
+    AI: false,
+    id: '0',
+}
 
   function userGuess(tweet: TweetConfig, userAns: string) {
     localStorage.setItem("tweetData", JSON.stringify(tweets))
