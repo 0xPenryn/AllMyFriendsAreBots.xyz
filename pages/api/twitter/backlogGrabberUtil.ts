eb24790b3f24332f92d24594cc4a858167fea045
0xPenryn <penryn@pm.me>
2023-03-05 16:17:03 -0600

    fetch more tweets and try min_faves operator

--- pages/api/twitter/backlogGrabberUtil.ts
+++ pages/api/twitter/backlogGrabberUtil.ts
@@ -193,7 +193,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
   var tweetSearch;
 
-    tweetSearch = await client.v2.search('ai', {
+    tweetSearch = await client.v2.search('ai min_faves:500', {
       'tweet.fields': ['attachments', 'author_id', 'conversation_id', 'created_at', 'id', 'in_reply_to_user_id', 'lang', 'possibly_sensitive', 'referenced_tweets', 'source', 'text', 'withheld', 'public_metrics'],
       expansions: ['attachments.media_keys', 'attachments.poll_ids', 'referenced_tweets.id', 'author_id', 'entities.mentions.username', 'geo.place_id', 'in_reply_to_user_id', 'referenced_tweets.id.author_id'],
       'media.fields': ['url'],
@@ -201,6 +201,26 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       'sort_order': 'relevancy',
     })
 
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
+    tweetSearch.fetchNext()
     tweetSearch.fetchNext()
     tweetSearch.fetchNext()
     tweetSearch.fetchNext()
