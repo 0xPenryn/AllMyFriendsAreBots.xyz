aca68d74213be4e5d4d07b78cbd297de67257414
0xPenryn <penryn@pm.me>
2023-03-06 13:05:46 -0600

    Update pregame.tsx

--- pages/pregame.tsx
+++ pages/pregame.tsx
@@ -66,7 +66,7 @@ const PreGame: NextPage = () => {
               clearState();
               signOut({ callbackUrl: "/" });
             }}>Sign out</button>
-            <img src={session.user.image!} className="h-10 mx-2.5 rounded-full" />
+            <img src={session.user.image!} className="h-10 ml-2.5 rounded-full" />
           </>}</div>
       </div>
       <div className="flex flex-col w-screen justify-center items-center text-center">
