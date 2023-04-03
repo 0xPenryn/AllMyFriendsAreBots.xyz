88b3a4022faa49eec01d0dbe96a3d2ddde6cd0b2
0xPenryn <penryn@pm.me>
2023-04-03 11:29:23 -0500

    add retries to tweetByUser

--- pages/api/twitter/tweetsByUser.ts
+++ pages/api/twitter/tweetsByUser.ts
@@ -1,5 +1,5 @@
 import { getToken } from 'next-auth/jwt';
-import { TwitterApi } from 'twitter-api-v2';
+import { TweetUserTimelineV2Paginator, TweetV2HomeTimelineParams, TwitterApi } from 'twitter-api-v2';
 import { TwitterV2IncludesHelper } from 'twitter-api-v2';
 import { parseTweet } from '../../../utils/tweetHelper';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
@@ -18,25 +18,35 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
   const client = new TwitterApi(token.access_token as string);
 
-  var tweetsOfUser;
+  var tweetsOfUser: TweetUserTimelineV2Paginator;
+
+  var timelineFields: TweetV2HomeTimelineParams = {
+    'tweet.fields': ['attachments', 'author_id', 'conversation_id', 'created_at', 'id', 'in_reply_to_user_id', 'lang', 'possibly_sensitive', 'referenced_tweets', 'source', 'text', 'withheld', 'public_metrics'],
+    expansions: ['attachments.media_keys', 'attachments.poll_ids', 'referenced_tweets.id', 'author_id', 'entities.mentions.username', 'geo.place_id', 'in_reply_to_user_id', 'referenced_tweets.id.author_id'],
+    'media.fields': ['url'],
+    'user.fields': ['created_at', 'description', 'entities', 'id', 'location', 'name', 'pinned_tweet_id', 'profile_image_url', 'protected', 'public_metrics', 'url', 'username', 'verified', 'withheld'],
+    exclude: ['retweets', 'replies'],
+    // max_results: 10,
+  };
 
   if (req.body) {
-    tweetsOfUser = await client.v2.userTimeline(req.body.user_id,{
-        'tweet.fields': ['attachments', 'author_id', 'conversation_id', 'created_at', 'id', 'in_reply_to_user_id', 'lang', 'possibly_sensitive', 'referenced_tweets', 'source', 'text', 'withheld', 'public_metrics'],
-        expansions: ['attachments.media_keys', 'attachments.poll_ids', 'referenced_tweets.id', 'author_id', 'entities.mentions.username', 'geo.place_id', 'in_reply_to_user_id', 'referenced_tweets.id.author_id'],
-        'media.fields': ['url'],
-        'user.fields': ['created_at', 'description', 'entities', 'id', 'location', 'name', 'pinned_tweet_id', 'profile_image_url', 'protected', 'public_metrics', 'url', 'username', 'verified', 'withheld'],
-        exclude: ['retweets', 'replies'],
-        // max_results: 10,
-      });
-      console.log("tweetsOfUser:", tweetsOfUser);
+    for (var tries = 0; tries < 5; tries++) {
+      try {
+        console.log("trying to get user timeline, try number:", tries)
+        tweetsOfUser = await client.v2.userTimeline(req.body.user_id, timelineFields);
+      } catch (error) {
+        console.log("error getting user timeline, trying again");
+        continue;
+      }
+      break;
+    }
   } else {
     return res.status(418).json({ message: 'I\'m a teapot, and you didn\'t include a userID' })
   }
 
-  const includes = new TwitterV2IncludesHelper(tweetsOfUser);
+  const includes = new TwitterV2IncludesHelper(tweetsOfUser!);
 
-  for (const tweet of tweetsOfUser.tweets) {
+  for (const tweet of tweetsOfUser!.tweets) {
     // ignores tweets with polls, quotes, media, and by protected users
     if (!includes.poll(tweet) && !includes.quote(tweet) && !tweet.attachments && !includes.author(tweet)?.protected) {
       const parsedTweet = parseTweet({
